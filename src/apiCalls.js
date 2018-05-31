a1d53218a3b9dda40b7484b23ce3392ac6a9187c
Seamus Quinn <sumaesquinn@gmail.com>
2018-05-31 11:50:29 -0600

    Grab sworn members data and pass to card

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,8 +5,17 @@ export const fetchData = () => {
     .catch(error => console.log(error))
 }
 
-export const fetchSwornMembers = (house) => {
-  console.log(house)
+export const fetchSwornMembers = (urls) => {
+  const ids = urls.map(url => {
+    return url.split('/').pop()
+  })
+  const members = ids.map(id => {
+    let url = `http://localhost:3001/api/v1/character/${id}`
+    return fetch(url)
+      .then(response => response.json())
+      .catch(error => console.log(error))
+  })
+  return Promise.all(members)
 }
 
 
